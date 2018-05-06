75131031967b188d8b412b33bf594c9ee970c03f
mchpathak@gmail.com <mchpathak@gmail.com>
2018-05-06 20:35:25 +0530

    practice jar

---
+++ practicejar/src/main/java/org/com/interview/practice/ConfigurationPropertiesLoader.java
@@ -0,0 +1,47 @@
+package org.com.interview.practice;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class ConfigurationPropertiesLoader {
+
+	private static ConfigurationPropertiesLoader fileLoader = null;
+	
+	static final Properties PROPERTIES = new Properties();
+	
+	private ConfigurationPropertiesLoader() {
+	}
+	
+	public static ConfigurationPropertiesLoader getInstance(){
+		
+		if(fileLoader==null){
+			synchronized (ConfigurationPropertiesLoader.class) {
+               if(fileLoader==null){
+            	   fileLoader = new ConfigurationPropertiesLoader();
+               }				
+			}
+		}
+		return fileLoader;
+	}
+	
+	public void loadPropertiesFile(String path) throws Exception {
+		// TODO Auto-generated method stub
+         try {
+        	FileInputStream propertiesFile = new FileInputStream(new File(path)); 
+			PROPERTIES.load(propertiesFile);
+		} catch (IOException e) {
+			System.out.println("Please provide the absolute path for the file");
+			System.exit(1);
+		}
+	}
+	
+	
+	 public Enumeration<Object> getProperties() throws Exception {
+	     return PROPERTIES.keys();  
+	     
+	  }
+	
+}
