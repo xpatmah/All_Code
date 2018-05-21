6ce2a5430da47b74f849a9001f357fc0a0c0ec0d
mchpathak@gmail.com <mchpathak@gmail.com>
2018-05-22 03:05:02 +0530

    Normal changes

--- JUnitTestCases/src/test/java/com/org/test/unit/JUnitTestCases/GreetingImpleTest.java
+++ JUnitTestCases/src/test/java/com/org/test/unit/JUnitTestCases/GreetingImpleTest.java
@@ -3,18 +3,33 @@ package com.org.test.unit.JUnitTestCases;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GreetingImpleTest {
+
+	private GreetingImpl imp;
+
 	
-		@Test
-		public void greetings() {
-			GreetingImpl imp = new GreetingImpl();
-			String name = imp.getName("Ramesh");
-			assertNotNull(name);
-			assertEquals("hello Ramesh", name);
-		}
+	@Before
+	public void setup() {
+		imp = new GreetingImpl();
+	}
+
 	
+	@Test
+	public void greetings() {
+		String name = imp.getName("Ramesh");
+		assertNotNull(name);
+		assertEquals("hello Ramesh", name);
+	}
 	
+// Mocking is test in isolation
 	
+	@After
+	public void cleanUp() {
+		imp=null;
+	}
+
 }
