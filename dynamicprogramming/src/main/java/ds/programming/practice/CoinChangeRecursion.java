a74f98ebc114910fa5d7d22352fbc69504aa0ebb
mchpathak@gmail.com <mchpathak@gmail.com>
2018-06-12 23:31:41 +0530

    Data Structures

---
+++ dynamicprogramming/src/main/java/ds/programming/practice/CoinChangeRecursion.java
@@ -0,0 +1,36 @@
+package ds.programming.practice;
+
+public class CoinChangeRecursion {
+
+	public static void main(String[] args) {
+	
+		int[] coins = {1,2,3};
+		
+		CoinChangeRecursion coin = new CoinChangeRecursion();
+		int totalCombinations = coin.coinChange(coins, 3, 4);
+		System.out.println(totalCombinations);
+	}
+	
+	
+	public int coinChange(int[] coins , int m , int n) {
+		
+		if(n==0) {
+			return 1;
+		}
+		
+		if(n< 0) {
+			return 0;
+		}
+		
+		if(m<=0 && n > 0) {
+			return 0;
+		}
+		
+		return coinChange(coins, m, n-coins[m-1]) + coinChange(coins, m-1, n);
+		
+	}
+	
+	
+	
+	
+}
