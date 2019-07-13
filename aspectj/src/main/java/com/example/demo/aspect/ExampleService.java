20dc6d35a10076cbb513882bb9d1e1b9f7a91e20
xpatmah <mchpathak@gmail.com>
2019-07-13 15:37:51 +0530

    aspect j proxy

--- aspectj/src/main/java/com/example/demo/aspect/ExampleService.java
+++ aspectj/src/main/java/com/example/demo/aspect/ExampleService.java
@@ -5,26 +5,35 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Example {
+public class ExampleService {
 
 	@Autowired
 	private HandlerClass handlerClass;
+	
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier(value="localRunnabl") private Runnable localRunnable;
+	 */
+	
 	
 	@RequestMapping("/home")
 	String home() {
 		ExecutorService service = Executors.newFixedThreadPool(10);
 		for(int i = 0 ; i <10;i++) {
-			service.submit(new Runnable() {
-				@Override
-				public void run() {
-					handlerClass.mainMethod();	
-				}
-			});
+			
+			  service.submit(new Runnable() {
+			  
+			  @Override public void run() { handlerClass.mainMethod(); } });
+			 
+			/* service.submit(localRunnable); */
+			//localRunnable.run();
 		}
 		return "Hello World!";
 	}
